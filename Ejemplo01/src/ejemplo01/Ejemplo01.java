ef113071ba95ef269b8c02e6785368ba5f275af6
alexsan11 <alexjsancu@gmail.com>
2022-11-02 15:54:04 -0500

    semana04

--- Ejemplo01/src/ejemplo01/Ejemplo01.java
+++ Ejemplo01/src/ejemplo01/Ejemplo01.java
@@ -17,8 +17,12 @@ public class Ejemplo01 {
     public static void main(String[] args) {
         // Ejemplo 
         String nombreEstudiante;
-        nombreEstudiante = "René Elizalde";
+        nombreEstudiante = "Alex Sánchez";
+        System.out.println(nombreEstudiante);
+        System.out.println("Alex Sánchez");
+        nombreEstudiante = "Cristian Espinoza";
         System.out.println(nombreEstudiante);
     }
-    
 }
+
+
